d949c81f18c3e73a2e112caedac33b70d0d14ad2
Subhajit Roy <subhajitroy005@gmail.com>
2023-05-09 10:06:18 +0530

    LED Blink example added.

---
+++ Examples/LED_Blink/RTOS_1/Debug/Third_Party/Free_RTOS/GCC/ARM_CM4F/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Third_Party/Free_RTOS/GCC/ARM_CM4F/port.c 
+
+OBJS += \
+./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.o 
+
+C_DEPS += \
+./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Third_Party/Free_RTOS/GCC/ARM_CM4F/%.o Third_Party/Free_RTOS/GCC/ARM_CM4F/%.su Third_Party/Free_RTOS/GCC/ARM_CM4F/%.cyclo: ../Third_Party/Free_RTOS/GCC/ARM_CM4F/%.c Third_Party/Free_RTOS/GCC/ARM_CM4F/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../USB_HOST/App -I"/home/subhajitroy005/sr005/projects/Free_RTOS/RTOS_Projects/RTOS_1/Third_Party" -I"/home/subhajitroy005/sr005/projects/Free_RTOS/RTOS_Projects/RTOS_1/Third_Party/Free_RTOS/GCC/ARM_CM4F" -I"/home/subhajitroy005/sr005/projects/Free_RTOS/RTOS_Projects/RTOS_1/Third_Party/Free_RTOS/include" -I"/home/subhajitroy005/sr005/projects/Free_RTOS/RTOS_Projects/RTOS_1/Third_Party/Free_RTOS/MemMang" -I../USB_HOST/Target -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Middlewares/ST/STM32_USB_Host_Library/Core/Inc -I../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Third_Party-2f-Free_RTOS-2f-GCC-2f-ARM_CM4F
+
+clean-Third_Party-2f-Free_RTOS-2f-GCC-2f-ARM_CM4F:
+	-$(RM) ./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.cyclo ./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.d ./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.o ./Third_Party/Free_RTOS/GCC/ARM_CM4F/port.su
+
+.PHONY: clean-Third_Party-2f-Free_RTOS-2f-GCC-2f-ARM_CM4F
+
